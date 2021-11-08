854774b86a14d0f3cace216df4b227634879ea35
Izzy <snakedreamz134@gmail.com>
2021-11-08 13:45:28 -0500

    added background image, fixed description text.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,16 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-router.get("/hello/world", function (req, res) {
+router.get("/", function (req, res) {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
 });
+
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
